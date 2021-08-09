32070008fbe0aa38753c1528664cbc7734043aab
jacs4210 <jairo.cuartas18@gmail.com>
2021-08-08 19:13:46 -0500

    Initial commit
    Create image for go app - Hi World

---
+++ hello.go
@@ -0,0 +1,11 @@
+// Definición del paquete.
+package main
+
+// Import del paquete fmt
+import "fmt"
+
+// Se define función principal
+func main() {
+	// Uso de la función Println del paquete fmt
+	fmt.Println("Hi World")
+}
